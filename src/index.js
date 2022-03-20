cb59feee84aaaaa65b3abb388341474028c22844
Amit Chattopadhyay <88921028+Amitch07@users.noreply.github.com>
2022-03-20 09:57:56 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const d = new Date();
+const time = d.getHours();
+console.log(d);
+const stl = {
+  color: "",
+  greeting: ""
+};
+if (time < 12) {
+  stl.color = "red";
+  stl.greeting = "Good Morning";
+} else if (time < 18) {
+  stl.color = "Green";
+  stl.greeting = "Good Afternoon";
+} else {
+  stl.color = "blue";
+  stl.greeting = "Good Night";
+}
+ReactDOM.render(
+  <h1 style={stl} className="heading">
+    {stl.greeting + "Amit today is : " + d}
+  </h1>,
+  document.getElementById("root")
+);
